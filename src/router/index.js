da0e58b03cc631bc806e4fadbb40d46c13bcfb8c
wangs <993752649@qq.com>
2019-10-10 15:01:49 +0800

    vue demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import FirstPage from '@/components/FirstPage'
+import SecondPage from '@/components/SecondPage'
+import FirstPart from '@/components/HomeComponents/FirstPart'
+import SecondPart from '@/components/HomeComponents/SecondPart'
 
 Vue.use(Router)
 
@@ -10,6 +15,33 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'FirstPart',
+          component: FirstPart
+        },
+        {
+          path: 'SecondPart',
+          name: 'SecondPart',
+          component: SecondPart
+        }
+      ]
+    },
+    {
+      path: '/FirstPage',
+      name: 'FirstPage',
+      component: FirstPage
+    },
+    {
+      path: '/SecondPage',
+      name: 'SecondPage',
+      component: SecondPage
     }
   ]
 })
